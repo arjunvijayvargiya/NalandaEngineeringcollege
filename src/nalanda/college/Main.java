ad2ff4039f90e20caf306044b430974e14e8f2e3
arvijayv <arvijayv@ARVIJAYV-LAP.oradev.oraclecorp.com>
2017-07-26 17:17:22 +0530

    added marks

--- src/nalanda/college/Main.java
+++ src/nalanda/college/Main.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package nalanda.college;
 
 /**
@@ -10,5 +6,8 @@ package nalanda.college;
  * @author arvijayv
  */
 public class Main {
+    public static void main(String[] args) {
+               
+    }
     
 }
